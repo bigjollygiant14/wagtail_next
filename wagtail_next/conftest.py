960c7d8c652afc959ed284e463284673944b1bf7
bigjollygiant14 <jaywa14@gmail.com>
2025-02-19 20:10:43 -0700

    init

---
+++ wagtail_next/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from wagtail_next.users.models import User
+from wagtail_next.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
